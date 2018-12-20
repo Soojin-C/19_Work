c9ff49ebee783a933b8ac73444fd2c234cb7676c
Soojin Choi <schoi4@stuy.edu>
2018-12-20 04:34:57 -0500

    hw

--- client.c
+++ client.c
@@ -7,4 +7,17 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+
+  while (1){
+    printf("Input Data: ");
+    char input[BUFFER_SIZE];
+    char output[BUFFER_SIZE];
+    fgets(input, BUFFER_SIZE, stdin);
+
+    int writing = write(to_server, input, BUFFER_SIZE);
+    int reading = read(from_server, output, BUFFER_SIZE);
+
+    printf("server msg:[%s]\n", output);
+
+  }
 }
