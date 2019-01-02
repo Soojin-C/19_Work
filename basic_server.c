846023f8726e0f1678f9d6abc7d5c91a3af1cc33
Soojin Choi <schoi4@stuy.edu>
2019-01-02 02:09:17 -0500

    completed

--- basic_server.c
+++ basic_server.c
@@ -16,25 +16,31 @@ int main() {
 
   from_client = server_handshake( &to_client );
 
-  while(1){
+//  while(1){
     char input[BUFFER_SIZE] = "message recieved";
     char output[BUFFER_SIZE];
 
     int reading = read(from_client, output, BUFFER_SIZE);
-    //printf("%c", output[0]);
-/*
+    printf("%c", output[0]);
+
     int counter = 0;
-    while (counter <= BUFFER_SIZE){
-      output[counter] = output[counter] + 3;
-      printf("temp: [%c]", output[counter]);
-      counter++;
+    while (counter < strlen(output)){
+
+      char curr = output[counter];
+      if(!strcmp(&curr, "\n")){
+        output[counter] = 0;
+        //printf("%s\n", output);
+      }
+
+      output[counter] = curr + 3;
+      counter ++;
+
     }
-*/
-    //printf("msg : %s", output);
+    printf("output: %s\n", output);
 
     int writing = write(to_client, output, BUFFER_SIZE);
 
     close(to_client);
     close(from_client);
-  }
+  //}
 }
